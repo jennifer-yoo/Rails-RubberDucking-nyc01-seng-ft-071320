c9adc9e9f863e60b715b46bdb03095a3f6f64685
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-08-11 01:01:49 -0400

    DONE

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,9 @@
 class Duck < ApplicationRecord
+    belongs_to :student
+    validates :name, :description, presence: true
+
+
+    def my_students
+        Duck.all.select { |duck| duck.student_id == self.id }
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+validates :name, presence: true
+
+    def my_ducks
+        Duck.all.select { |duck| duck.student_id == self.id}
+    end
 end
